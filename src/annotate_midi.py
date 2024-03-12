c2e11d628098d8725105a426dde7177a29f817fa
jingx <xujingxin07@gmail.com>
2024-03-12 14:51:23 +0800

    Found secondsMap property. Going to delete previous work. Leaving a record.

--- src/annotate_midi.py
+++ src/annotate_midi.py
@@ -1,56 +1,28 @@
 from music21 import *
 
-midi_stream = converter.parse("../Sonate_No._14_Moonlight_1st_Movement.mid")
-metronome_iterator = midi_stream.recurse().getElementsByClass(tempo.MetronomeMark)
-if len(metronome_iterator.getElementsByOffset(0)) == 0:
-    raise ValueError("invalid midi: no MetronomeMark at the beginning")
+def met_list(input_midi):
+    metronome_iterator = input_midi.flatten().getElementsByClass(tempo.MetronomeMark)
+    if len(metronome_iterator.getElementsByOffset(0)) == 0:
+        raise ValueError("invalid midi: no MetronomeMark at the beginning")
+    return sorted(metronome_iterator, key=lambda x: x.offset)
 
-measure_iterator = midi_stream.recurse().getElementsByClass(stream.base.Measure)
-met_list = list()
-for mm in metronome_iterator:
-    met_list.append([mm.offset, mm.secondsPerQuarter()])
-    met_list = sorted(met_list)
-met_list.append([4.0, 2.49])
-met_list.append([20, 30])
-print(met_list)
+def find_measure_offsets(metlist, stream):
+    measure_iterator = stream.recurse().getElementsByClass(stream.base.Measure)
+    offset_seconds_dict = dict()
+    next_index = 0
+    next_offset = 0
+    current_duration_seconds = 0
+    last_index = len(met_list) - 1
+    print(last_index)
+    for measure in measure_iterator:
+        if measure.offset >= next_offset:  # change of MetronomeMark
+            current_secondsPerQuarter = met_list[next_index][1]
+            next_index += 1
+            if next_index == last_index:
+                pass
+            else:
+                next_offset = met_list[next_index][0]
 
-offset_seconds_dict = dict()
-next_index = 0
-next_offset = 0
-last_index = len(met_list)-1
-print(last_index)
-for measure in measure_iterator:
-    if measure.offset >= next_offset:
-        current_secondsPerQuarter = met_list[next_index][1]
-        next_index += 1
-        if next_index == last_index:
-            pass
-        else:
-            next_offset = met_list[next_index][0]
-
-    offset_seconds_dict[measure.measureNumber] = measure.offset * current_secondsPerQuarter
-    print(offset_seconds_dict)
-
-
-# class AnnotatedMidi():
-#     def check_stream(self):  # check that there is a metronome mark at offset 0
-#         if len(self.metronome_iterator.getElementsByOffset(0)) != 0:
-#             pass
-#         else:
-#             raise ValueError("invalid midi: no MetronomeMark at the beginning")
-#
-#     def __init__(self, midi_stream):
-#         self.metronome_iterator = midi_stream.recurse().getElementsByClass(tempo.MetronomeMark)
-#         self.measure_iterator = midi_stream.recurse().getElementsByClass(stream.base.Measure)
-#         met_list = list()
-#         current_metronome_index = 0
-#         self.check_stream()
-#
-#     def update_met_list(self):
-#         for mm in self.metronome_iterator:
-#             self.met_list.append([mm.offset, mm.secondsPerQuarter()])
-#         self.met_list = sorted(self.met_list)
-#         print(self.met_list)
-
-
-
+        current_duration_seconds += current_secondsPerQuarter  # * quarters in a measure
+        offset_seconds_dict[measure.measureNumber] = current_duration_seconds
+        print(offset_seconds_dict)
--- src/rough_work.py
+++ src/rough_work.py
@@ -1,3 +1,21 @@
-from music21 import stream, tempo
-a_stream = stream.Stream()
-print(len(a_stream.getElementsByOffset(0).getElementsByClass(tempo.MetronomeMark)))
+from music21 import stream, tempo, note, converter
+
+# midi_stream = converter.parse("../Sonate_No._14_Moonlight_1st_Movement.mid")
+
+strm = stream.Stream([
+    stream.Part([
+        stream.Measure(note.Note(type='whole'), number=1, offset=0),
+        stream.Measure(number=2, offset=4)
+    ], offset=20)
+])
+strm.show('text')
+map = strm.recurse().getElementsByClass(stream.base.Measure).secondsMap
+print(map)
+
+# p0 = stream.Part(id='part0')
+# m01 = stream.Measure(number=1)
+# m01.append(note.Note('C', type='whole'))
+# m02 = stream.Measure(number=2)
+# m02.append(note.Note('D', type='whole'))
+# p0.append([m01, m02])
+# p0.show('text')
