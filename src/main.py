9d2a207d1fb490f26a2535bd16061ae657fcf724
jingx <xujingxin07@gmail.com>
2024-03-20 21:29:53 +0800

    Sort of working. Fixing bugs. Still on the case of wrong order only. Haven't put max no. of extensions.

--- src/main.py
+++ src/main.py
@@ -1,21 +1,48 @@
+import random
+
 from music21 import *
+from collections import Counter
 
 midi = (converter.parse('../Sonate_No._14_Moonlight_1st_Movement.mxl')
-        .flatten().getElementsByClass(note.NotRest))
-def get_seconds(midi_stream):
-    return midi_stream.secondsMap
+        .flatten().getElementsByClass(note.NotRest)).stream()
 
-mxl = converter.parse('../Sonate_No._14_Moonlight_1st_Movement.mxl').recurse().getElementsByClass(note.NotRest)
+mxl = (converter.parse('../Sonate_No._14_Moonlight_1st_Movement.mxl')
+       .flatten().getElementsByClass(note.NotRest)).stream()
 
-midi.show('text')
-print('----------------------------------------')
-mxl.show('text')
-for i in range(10):
-    midi_i = midi.getElementsByOffset(midi[i].offset)  # need to code such that elements with offsets close to but not equal are also put together
-    mxl_i = midi.getElementsByOffset(midi[i].offset)
-    print(midi_i.elements)
-    print(mxl_i.elements)
-    if midi_i.elements == mxl_i.elements:
+
+def get_seconds(element) -> float:
+    midi_dict_list = midi.secondsMap
+    for x in midi_dict_list:
+        if x['element'] == element: return x['endTimeSeconds']
+    return 'NOT FOUND!!!'
+
+
+record = []
+
+
+def match(midi_snip, mxl_snip) -> tuple:
+    if Counter(map(str, midi_snip)) == Counter(map(str, mxl_snip)):
         print('matched')
-    else:
-        print('not matched')
+        print(midi_snip, mxl_snip)
+        # something to get seconds
+        mxl_snip[-1].__setattr__('secrsss', get_seconds(midi_snip[-1]))
+        record.append(mxl_snip[-1])
+        print("end seconds: ", mxl_snip[-1].secrsss)
+        print("record: ", record)
+
+        return mxl_snip[-1].next(), midi_snip[-1].next()
+
+    else:  # expands the range
+        print(midi_snip, type(midi_snip))
+        midi_snip.append(midi_snip[-1].next())
+        mxl_snip.append(mxl_snip[-1].next())
+        print('expanding')
+        return midi_snip, mxl_snip
+
+
+def iter(midi_snip, mxl_snip):
+    while mxl_snip[0].offset <= mxl.last().offset:
+        mxl_snip, midi_snip = match(mxl_snip, midi_snip)
+
+
+iter(midi_snip=[midi[0]], mxl_snip=[mxl[0]])
--- src/rough_work.py
+++ src/rough_work.py
@@ -1,12 +1,10 @@
-from music21 import stream, tempo, note, converter
+from collections import Counter
 
-midi_stream = converter.parse("../Sonate_No._14_Moonlight_1st_Movement.mid")
-print(midi_stream.recurse().matchingElements())
+import music21.corpus.chorales
+from music21 import stream, note, corpus, converter
 
-# p0 = stream.Part(id='part0')
-# m01 = stream.Measure(number=1)
-# m01.append(note.Note('C', type='whole'))
-# m02 = stream.Measure(number=2)
-# m02.append(note.Note('D', type='whole'))
-# p0.append([m01, m02])
-# p0.show('text')
+midi = (converter.parse('../Sonate_No._14_Moonlight_1st_Movement.mxl')
+        .flatten().getElementsByClass(note.NotRest))
+
+txt = open('secondsMap.json', 'w')
+txt.write(str(midi.stream().secondsMap))
